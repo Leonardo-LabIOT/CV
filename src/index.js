a6c6dc8e1098b1d96e012bcd5a8a7f2b80cf542f
LeoIOT <leonardo_pereira1989@hotmail.com>
2022-03-18 20:00:23 -0300

    V0.02

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// var, const, let;
+
+let a="Olá Mundo";
+
+
+// alert(b);
+document.getElementById('demo').innerHTML = 'Hello JavaScript';
+
+{/* <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button> */}
+function myFun(){
+    alert(a);
+}
